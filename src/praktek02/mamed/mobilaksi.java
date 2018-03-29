b337dcda0991771af910ed9985a6059b3810fef7
mamed <akhyatf706@gmail.com>
2018-03-29 12:44:16 +0800

    mengisi nilai atribut

--- src/praktek02/mamed/mobilaksi.java
+++ src/praktek02/mamed/mobilaksi.java
@@ -3,6 +3,8 @@ package praktek02.mamed;
 public class mobilaksi {
     public static void main(String[] args) {
         mobil Q1 = new mobil();
+        Q1.harga = 900;
+        Q1.jumlah = 5;
     }
     
 }
