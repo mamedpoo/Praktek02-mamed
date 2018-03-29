035800ef74ce2c7df187a7e603de817beca811a9
mamed <akhyatf706@gmail.com>
2018-03-29 12:28:03 +0800

    Initial commit

---
+++ src/praktek02/mamed/Praktek02Mamed.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek02.mamed;
+
+/**
+ *
+ * @author Akhyat
+ */
+public class Praktek02Mamed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
