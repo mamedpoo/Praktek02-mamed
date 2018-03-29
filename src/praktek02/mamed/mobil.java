74d3374fca78bae1e94df827112f73e1e5d56b5a
mamed <akhyatf706@gmail.com>
2018-03-29 12:34:42 +0800

    membuat attribut dan behavior

--- src/praktek02/mamed/mobil.java
+++ src/praktek02/mamed/mobil.java
@@ -1,5 +1,14 @@
 package praktek02.mamed;
 
 public class mobil {
+    double harga;
+    double jumlah;
     
+    void transportasi(){
+        System.out.println("=========================");
+        System.out.println("Harga           :"+harga);
+        System.out.println("Jumlah          :"+jumlah);
+        System.out.println("=========================");
+    
+    }
 }
